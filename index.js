7dcaa4f3befd1d49e6d35913f6cd8d54fdad2529
Tushar <tusharshingane1234@gmail.com>
2022-08-30 18:22:36 +0530

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const path = require("path");
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 const db = require('./config/mongoose');
 const Contact = require("./models/contact");
